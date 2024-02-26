8d9f250707e93ac836aa43c84d7c3bd15b07e3bf
Deivid Veloza <velozadeivid@gmail.com>
2024-02-26 15:20:01 -0500

    Delete Copia directory

--- Copia/src/context/apis.js
+++
@@ -1,9 +0,0 @@
-
-//Desarollo
-
-/* export const wsp_api = `http://192.168.1.3/simplexweb/tickets/wsp_api/wsp_api.php`;
-export const webhook = `http://192.168.1.3/simplexweb/tickets/webhook/class_envio_mensajes`;
- */
-//Produccion
- export const wsp_api = `https://api.exiware.co/wsp_api/wsp_api.php`;
- export const webhook = `https://api.exiware.co/webhook/class_envio_mensajes`;
